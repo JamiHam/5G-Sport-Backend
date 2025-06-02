4c37b4d64f647fd6526f75ee4c1b6b872860bfed
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-02 14:55:58 +0300

    Changed consumer implementation to run as a thread

--- src/main/java/org/example/Consumer.java
+++ src/main/java/org/example/Consumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import org.slf4j.Logger;
@@ -13,31 +14,49 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-public class Consumer {
-    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+public class Consumer implements Runnable {
+    private final Logger log = LoggerFactory.getLogger(Consumer.class);
+    private final KafkaConsumer<String, String> consumer;
 
-    public static void main(String[] args) {
-        String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "5G-Sport";
-        String topic = "sensor_data";
+    public Consumer(String bootstrapServer, String groupId, String topic) {
+        consumer = setupKafkaConsumer(bootstrapServer, groupId);
+        consumer.subscribe(Arrays.asList(topic));
+    }
 
+    public void run() {
+        try {
+            // poll for new data
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            // This exception is expected when closing a consumer, no error handling required.
+            log.info("Wake up exception");
+        } catch (Exception e) {
+            log.error("Unexpected exception: ", e);
+        } finally {
+            consumer.close(); // this will also commit the offsets if necessary.
+            log.info("Consumer closed successfully");
+        }
+    }
+
+    public void shutDown() {
+        consumer.wakeup();
+    }
+
+    private KafkaConsumer<String, String> setupKafkaConsumer(String bootstrapServer, String groupId) {
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(Arrays.asList(topic));
-
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-
-            for (ConsumerRecord<String, String> record : records) {
-                log.info("Key: " + record.key() + ", Value: " + record.value());
-                log.info("Partition: " + record.partition() + ", Offset:" + record.offset());
-            }
-        }
+        return new KafkaConsumer<>(properties);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger log = LoggerFactory.getLogger(Main.class);
+
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "5G-Sport";
+        String topic = "sensor_data";
+
+        log.info("Creating the consumer thread");
+        Consumer consumer = new Consumer(bootstrapServer, groupId, topic);
+
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
+
+        // Add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Detected a shutdown");
+            consumer.shutDown();
+        }));
     }
 }
