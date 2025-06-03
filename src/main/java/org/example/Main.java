4d50f0712eee4da41972194dfb4c46bfcf7292ac
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-03 13:57:43 +0300

    Moved Consumer and Producer inside kafka package

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.kafka.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- src/main/java/org/example/kafka/Consumer.java
+++ src/main/java/org/example/kafka/Consumer.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
