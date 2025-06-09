b93b83a7730827c684a5322a1092ffb070f767e9
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-09 15:00:18 +0300

    Created a new kafka producer implementation with spring-kafka

---
+++ src/main/java/org/example/kafka/KafkaProducer.java
@@ -0,0 +1,15 @@
+package org.example.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("sensor_data", message);
+    }
+}
