f444c72e6c737a495636650d0e659c664591733b
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-09 15:05:50 +0300

    Application.java now uses KafkaProducer to send a message on startup (to test KafkaConsumer)

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import org.example.kafka.KafkaProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        KafkaProducer producer = context.getBean(KafkaProducer.class);
+        producer.sendMessage("Hello world");
     }
 }
