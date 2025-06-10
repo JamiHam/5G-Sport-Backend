805550927e1259c05dc7adbe7ef02b549cf805ce
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-10 13:22:00 +0300

    WebSocketHandler-object is now saved to a variable, accessible with getter

--- src/main/java/org/example/WebSocketConfig.java
+++ src/main/java/org/example/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -8,10 +9,20 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    WebSocketHandler webSocketHandler;
+
+    public WebSocketConfig() {
+        webSocketHandler = new WebSocketHandler();
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry
-                .addHandler(new WebSocketConnectionHandler(), "/hello")
+                .addHandler(webSocketHandler, "/")
                 .setAllowedOrigins("*");
+    }
+
+    public WebSocketHandler getWebSocketHandler() {
+        return webSocketHandler;
     }
 }
