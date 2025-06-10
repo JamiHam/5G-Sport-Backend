8baa74a8d191478fe144184b2e0ce40d43a0cccb
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-10 13:29:10 +0300

    WebSocketHandler instance is now passed to Kafka consumer in Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.kafka.KafkaConsumer;
 import org.example.kafka.KafkaProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +8,12 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        WebSocketHandler webSocketHandler = context.getBean(WebSocketConfig.class).getWebSocketHandler();
+        KafkaConsumer consumer = context.getBean(KafkaConsumer.class);
+        consumer.setWebSocketHandler(webSocketHandler);
 
         KafkaProducer producer = context.getBean(KafkaProducer.class);
         producer.sendMessage("Hello world");
