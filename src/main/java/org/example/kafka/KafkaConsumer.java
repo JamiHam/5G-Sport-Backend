4ac22d707bf6bb80d4fb7635e7a3c0cc3d156061
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-10 13:25:24 +0300

    Kafka consumer now calls WebSocketHandlers broadcast method

--- src/main/java/org/example/kafka/KafkaConsumer.java
+++ src/main/java/org/example/kafka/KafkaConsumer.java
@@ -1,12 +1,25 @@
 package org.example.kafka;
 
+import org.example.WebSocketHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+
+import java.io.IOException;
 
 @Service
 public class KafkaConsumer {
+    WebSocketHandler webSocketHandler;
+
     @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.group-id}")
-    public void consume(String message) {
-        System.out.println(message);
+    public void consume(String message) throws IOException {
+        if (webSocketHandler != null) {
+            TextMessage convertedMessage = new TextMessage(message);
+            webSocketHandler.broadcast(convertedMessage);
+        }
+    }
+
+    public void setWebSocketHandler(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
     }
 }
