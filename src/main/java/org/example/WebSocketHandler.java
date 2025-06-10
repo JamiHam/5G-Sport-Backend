133e3137084e1a5dedd84b7019929c9460365a97
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-10 13:20:23 +0300

    Changed handleMessage() method to handleTextMessage()

--- src/main/java/org/example/WebSocketHandler.java
+++ src/main/java/org/example/WebSocketHandler.java
@@ -1,8 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@Component
 public class WebSocketHandler extends TextWebSocketHandler {
     List<WebSocketSession> webSocketSessions = Collections.synchronizedList(new ArrayList<>());
 
@@ -29,8 +30,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
     }
 
     @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        super.handleMessage(session, message);
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         for (WebSocketSession webSocketSession : webSocketSessions) {
             if (webSocketSession != session) {
                 webSocketSession.sendMessage(message);
