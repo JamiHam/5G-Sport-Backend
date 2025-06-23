cf04840ec855b2d85f85ad37bf7dbeb18465e3f9
Jami Hämäläinen <jami.hamala@gmail.com>
2025-06-23 15:23:12 +0300

    Application.java now generates sample data for testing

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.example.kafka.KafkaProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +10,14 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, JsonProcessingException {
         ApplicationContext context = SpringApplication.run(Application.class, args);
 
+        SensorData data = new SensorData();
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(data);
+
         KafkaProducer producer = context.getBean(KafkaProducer.class);
-        producer.sendMessage("Hello world");
+        producer.sendMessage(json);
     }
 }
