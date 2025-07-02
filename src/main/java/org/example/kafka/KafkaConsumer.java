45c393939d231c1698c6cec7fb5a28281c3a70c3
Jami Hämäläinen <jami.hamala@gmail.com>
2025-07-02 13:04:27 +0300

    Replaced prints with logger messages in KafkaConsumer and KafkaProducer

--- src/main/java/org/example/kafka/KafkaConsumer.java
+++ src/main/java/org/example/kafka/KafkaConsumer.java
@@ -1,23 +1,49 @@
 package org.example.kafka;
 
-import org.example.WebSocketHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.example.websocket.WebSocketHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 
 @Service
 public class KafkaConsumer {
-    @Autowired
-    private WebSocketHandler webSocketHandler;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private WebSocketHandler handler = null;
+    private CountDownLatch latch = new CountDownLatch(1);
+    private String payload;
 
     @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.group-id}")
     public void consume(String message) throws IOException {
-        if (webSocketHandler != null) {
+        LOGGER.info("Received payload = '{}'", message);
+
+        if (handler != null) {
             TextMessage convertedMessage = new TextMessage(message);
-            webSocketHandler.broadcast(convertedMessage);
+            handler.broadcast(convertedMessage);
         }
+
+        payload = message;
+        latch.countDown();
+    }
+
+    public void resetLatch() {
+        latch = new CountDownLatch(1);
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setHandler(WebSocketHandler handler) {
+        this.handler = handler;
     }
 }
