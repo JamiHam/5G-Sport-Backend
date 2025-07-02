88e2aa9c7a10d41871f76bc90521e92bdc92cd3d
Jami Hämäläinen <jami.hamala@gmail.com>
2025-07-02 12:59:17 +0300

    Created websocket package and renamed WebSocketConnectionTest to WebSocketIntegrationTest

--- src/main/java/org/example/websocket/WebSocketHandler.java
+++ src/main/java/org/example/websocket/WebSocketHandler.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.kafka.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,21 +16,24 @@ import java.util.List;
 
 @Service
 public class WebSocketHandler extends TextWebSocketHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketHandler.class);
+
     private List<WebSocketSession> sessions = Collections.synchronizedList(new ArrayList<>());
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println(session.getId() + " connected");
+        LOGGER.info("WebSocket session '{}' connected", session.getId());
         sessions.add(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        System.out.println(session.getId() + " disconnected");
+        LOGGER.info("WebSocket session '{}' disconnected", session.getId());
         sessions.remove(session);
     }
 
     public void broadcast(TextMessage message) throws IOException {
+        LOGGER.info("Broadcasting message to {} connected WebSocket session(s) = {}", sessions.size(), message);
         for (WebSocketSession webSocketSession : sessions) {
             webSocketSession.sendMessage(message);
         }
