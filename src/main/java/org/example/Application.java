a6ec76b87976190136f4f041641a185ee5465c6c
Jami Hämäläinen <jami.hamala@gmail.com>
2025-07-02 13:06:02 +0300

    Removed test messages

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,23 +1,18 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.example.kafka.KafkaProducer;
+import org.example.kafka.KafkaConsumer;
+import org.example.websocket.WebSocketHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws InterruptedException, JsonProcessingException {
+    public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
 
-        SensorData data = new SensorData();
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(data);
-
-        KafkaProducer producer = context.getBean(KafkaProducer.class);
-        producer.sendMessage(json);
+        KafkaConsumer consumer = context.getBean(KafkaConsumer.class);
+        WebSocketHandler handler = context.getBean(WebSocketHandler.class);
+        consumer.setHandler(handler);
     }
 }
