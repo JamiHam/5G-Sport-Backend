ef887f81863dc74f3e01a29a5336564fab8bca11
Jami Hämäläinen <jami.hamala@gmail.com>
2025-07-02 13:02:27 +0300

    Replaced prints with logger messages in WebSocketHandler

--- src/main/java/org/example/websocket/WebSocketHandler.java
+++ src/main/java/org/example/websocket/WebSocketHandler.java
@@ -1,6 +1,5 @@
-package org.example;
+package org.example.websocket;
 
-import org.example.kafka.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
