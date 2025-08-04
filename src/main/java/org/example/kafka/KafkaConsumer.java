73a44773fe33f0554c2d2c94dfff74247d3509aa
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-04 13:53:20 +0300

    KafkaConsumer now uses DataService to save data to database

--- src/main/java/org/example/kafka/KafkaConsumer.java
+++ src/main/java/org/example/kafka/KafkaConsumer.java
@@ -1,10 +1,14 @@
 package org.example.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.example.database.DataService;
 import org.example.websocket.WebSocketHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 
@@ -18,20 +22,45 @@ public class KafkaConsumer {
     @Autowired
     private WebSocketHandler handler;
 
+    @Autowired
+    private DataService dataService;
+
     private CountDownLatch latch = new CountDownLatch(1); // For testing.
     private String payload; // For testing.
 
     @KafkaListener(topics = "#{'${spring.kafka.topics}'.split(',')}", groupId = "${spring.kafka.group-id}")
-    public void consume(String message) throws IOException {
-        LOGGER.info("Received payload = '{}'", message);
+    public void consume(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) throws IOException {
+        LOGGER.info("Received message from topic '{}', payload = '{}'", topic, message);
 
+        broadcast(message);
+        saveToDatabase(message, topic);
+
+        payload = message;
+        latch.countDown();
+    }
+
+    private void broadcast(String message) throws IOException {
         if (handler != null) {
             TextMessage convertedMessage = new TextMessage(message);
             handler.broadcast(convertedMessage);
         }
+    }
 
-        payload = message;
-        latch.countDown();
+    private void saveToDatabase(String message, String topic) throws JsonProcessingException {
+        switch (topic) {
+            case "sensors.imu":
+                dataService.saveImuData(message);
+                break;
+            case "sensors.heart_rate":
+                dataService.saveHeartRateData(message);
+                break;
+            case "sensors.ecg":
+                dataService.saveEcgData(message);
+                break;
+            case "sensors.gnss":
+                dataService.saveGnssData(message);
+                break;
+        }
     }
 
     public void resetLatch() {
