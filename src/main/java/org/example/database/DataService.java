99e2f97037b5e772e98d90112c81cf603df1a7d8
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-06 15:14:42 +0300

    Created saveECGData() implementation

--- src/main/java/org/example/database/DataService.java
+++ src/main/java/org/example/database/DataService.java
@@ -2,10 +2,10 @@ package org.example.database;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.database.model.Gnss;
-import org.example.database.repository.GnssRepository;
-import org.example.database.repository.PicoRepository;
-import org.example.kafka.KafkaConsumer;
+import org.example.database.model.ECG;
+import org.example.database.model.ECGSample;
+import org.example.database.model.GNSS;
+import org.example.database.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +20,18 @@ public class DataService {
     PicoRepository picoRepository;
 
     @Autowired
-    GnssRepository gnssRepository;
+    MovesenseRepository movesenseRepository;
 
-    public void saveImuData(String message) {
+    @Autowired
+    ECGRepository ecgRepository;
+
+    @Autowired
+    ECGSampleRepository ecgSampleRepository;
+
+    @Autowired
+    GNSSRepository gnssRepository;
+
+    public void saveIMUData(String message) {
         System.out.println("saveImuData called");
     }
 
@@ -30,14 +39,28 @@ public class DataService {
         System.out.println("saveHeartRateData called");
     }
 
-    public void saveEcgData(String message) {
-        System.out.println("saveEcgData called");
+    public void saveECGData(String message) throws JsonProcessingException {
+        ECG ecg = objectMapper.readValue(message, ECG.class);
+        ecgRepository.save(ecg);
+
+        saveECGSamples(ecg);
+
+        LOGGER.info("ECG data saved to database = '{}'", message);
     }
 
-    public void saveGnssData(String message) throws JsonProcessingException {
-        Gnss gnss = objectMapper.readValue(message, Gnss.class);
+    public void saveGNSSData(String message) throws JsonProcessingException {
+        GNSS gnss = objectMapper.readValue(message, GNSS.class);
         gnssRepository.save(gnss);
 
         LOGGER.info("GNSS data saved to database = '{}'", message);
+    }
+
+    private void saveECGSamples(ECG ecg) {
+        for(int value : ecg.getSampleValues()) {
+            ECGSample sample = new ECGSample();
+            sample.setEcg(ecg);
+            sample.setValue(value);
+            ecgSampleRepository.save(sample);
+        }
     }
 }
