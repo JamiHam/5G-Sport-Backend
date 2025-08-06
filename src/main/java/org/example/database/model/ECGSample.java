ba0483b6950a12e8d8b0a53d35ca589a979710ee
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-06 15:12:21 +0300

    Created ECGSample entity

---
+++ src/main/java/org/example/database/model/ECGSample.java
@@ -0,0 +1,35 @@
+package org.example.database.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ecg_sample")
+public class ECGSample {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int value;
+
+    @ManyToOne
+    @JoinColumn(name = "ecg_id")
+    private ECG ecg;
+
+    public ECGSample() {}
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public ECG getEcg() {
+        return ecg;
+    }
+
+    public void setEcg(ECG ecg) {
+        this.ecg = ecg;
+    }
+}
