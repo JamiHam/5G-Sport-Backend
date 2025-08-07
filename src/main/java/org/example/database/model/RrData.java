b950864029959209b166d87533275a736c542889
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-07 13:48:55 +0300

    Created RrData entity

---
+++ src/main/java/org/example/database/model/RrData.java
@@ -0,0 +1,35 @@
+package org.example.database.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rr_data")
+public class RrData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int value;
+
+    @ManyToOne
+    @JoinColumn(name = "heart_rate_id")
+    private HeartRate heartRate;
+
+    public RrData() {}
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public HeartRate getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(HeartRate heartRate) {
+        this.heartRate = heartRate;
+    }
+}
