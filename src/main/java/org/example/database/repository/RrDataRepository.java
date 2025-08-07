e1bc9b11d20770325f90c8722418a22c76156697
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-07 13:49:33 +0300

    Created a repository for RrData entity

---
+++ src/main/java/org/example/database/repository/RrDataRepository.java
@@ -0,0 +1,7 @@
+package org.example.database.repository;
+
+import org.example.database.model.RrData;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RrDataRepository extends CrudRepository<RrData, Long> {
+}
