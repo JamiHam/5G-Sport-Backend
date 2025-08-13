bf5259d1bebf75ac89d884b105929a25332c84b9
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-13 11:43:17 +0300

    Created findByImuId() method

--- src/main/java/org/example/database/repository/IMUCoordinateRepository.java
+++ src/main/java/org/example/database/repository/IMUCoordinateRepository.java
@@ -3,5 +3,8 @@ package org.example.database.repository;
 import org.example.database.model.IMUCoordinate;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IMUCoordinateRepository extends CrudRepository<IMUCoordinate, Long> {
+    List<IMUCoordinate> findByImuId(long id);
 }
