05121337f034cb14d37790b2762f8505a7235ef4
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-13 11:42:41 +0300

    Created findByEcgId() method

--- src/main/java/org/example/database/repository/ECGSampleRepository.java
+++ src/main/java/org/example/database/repository/ECGSampleRepository.java
@@ -3,5 +3,8 @@ package org.example.database.repository;
 import org.example.database.model.ECGSample;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ECGSampleRepository extends CrudRepository<ECGSample, Long> {
+    List<ECGSample> findByEcgId(long id);
 }
