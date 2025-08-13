583bf799107f8737de0ee8818640eb3f24bb74b5
Jami Hämäläinen <jami.hamala@gmail.com>
2025-08-13 11:43:53 +0300

    Created findByHeartRateId() method

--- src/main/java/org/example/database/repository/RrDataRepository.java
+++ src/main/java/org/example/database/repository/RrDataRepository.java
@@ -3,5 +3,8 @@ package org.example.database.repository;
 import org.example.database.model.RrData;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RrDataRepository extends CrudRepository<RrData, Long> {
+    List<RrData> findByHeartRateId(long id);
 }
